6ed6b90530f3671dabef6d24bb6d1fa0078905ea
Matt Gleich <git@mattglei.ch>
2022-12-06 19:22:03 -0500

    feat: basic router
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- scraper/main.go
+++ scraper/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	r := mux.NewRouter()
+	r.HandleFunc("/strava", stravaEndpoint)
+	err := http.ListenAndServe(":80", r)
+	if err != nil {
+		log.Fatal("Failed to serve log", err)
+	}
+}
+
+func stravaEndpoint(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("strava endpoint hit")
 }
