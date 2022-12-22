7bfa4f8ed5b8c261e01cc3f8142cda1d975afa3e
Matt Gleich <git@mattglei.ch>
2022-12-22 18:45:52 -0500

    feat: handle challenge
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- scrapers/strava/src/main.rs
+++ scrapers/strava/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use challenge::Parameters;
+use dotenv::dotenv;
+use rocket::{self, launch, post, routes, serde::json::Json};
+
+mod challenge;
+
+#[launch]
+fn rocket() -> _ {
+    dotenv().expect("Failed to load dotenv");
+    rocket::build().mount("/", routes![endpoint])
+}
+
+#[post("/?<hub>")]
+pub fn endpoint(hub: Option<Parameters>) -> Json<Option<challenge::Response>> {
+    dbg!(&hub);
+    match hub {
+        Some(v) => Json(Some(challenge::Response {
+            challenge: v.challenge,
+        })),
+        None => Json(None),
+    }
 }
