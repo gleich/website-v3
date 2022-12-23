c92640c2ca38fe381c854ba2a081a6269ed56a90
Matt Gleich <git@mattglei.ch>
2022-12-22 19:58:12 -0500

    feat: logger and invalid verify_token
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- scrapers/strava/src/main.rs
+++ scrapers/strava/src/main.rs
@@ -1,11 +1,20 @@
+use std::env;
+
 use challenge::Parameters;
 use dotenv::dotenv;
 use rocket::{self, launch, post, routes, serde::json::Json};
+use tracing::{info, Level};
+use tracing_subscriber::FmtSubscriber;
 
 mod challenge;
 
 #[launch]
 fn rocket() -> _ {
+    let subscriber = FmtSubscriber::builder()
+        .with_max_level(Level::INFO)
+        .finish();
+    tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
+    info!("Booted");
     dotenv().expect("Failed to load dotenv");
     rocket::build().mount("/", routes![endpoint])
 }
@@ -14,9 +23,16 @@ fn rocket() -> _ {
 pub fn endpoint(hub: Option<Parameters>) -> Json<Option<challenge::Response>> {
     dbg!(&hub);
     match hub {
-        Some(v) => Json(Some(challenge::Response {
-            challenge: v.challenge,
-        })),
+        Some(v) => {
+            let verify_token = env::var("VERIFY_TOKEN").unwrap_or(String::new());
+            if v.verify_token != verify_token {
+                info!("Received invalid verify token of {}", verify_token);
+                return Json(None);
+            }
+            Json(Some(challenge::Response {
+                challenge: v.challenge,
+            }))
+        }
         None => Json(None),
     }
 }
