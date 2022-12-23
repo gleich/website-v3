510cf69ee2bb93ca2c5b73e9e92569a4a1d34e0b
Matt Gleich <git@mattglei.ch>
2022-12-23 18:00:57 -0500

    feat: reset to just regular program
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- scrapers/strava/src/main.rs
+++ scrapers/strava/src/main.rs
@@ -1,38 +1,17 @@
-use std::env;
-
-use challenge::Parameters;
-use dotenv::dotenv;
-use rocket::{self, launch, post, routes, serde::json::Json};
+use anyhow::{Context, Result};
 use tracing::{info, Level};
 use tracing_subscriber::FmtSubscriber;
 
-mod challenge;
+fn main() {
+    setup_logging().expect("setting up logging failed");
+    info!("booted");
+}
 
-#[launch]
-fn rocket() -> _ {
+fn setup_logging() -> Result<()> {
     let subscriber = FmtSubscriber::builder()
         .with_max_level(Level::INFO)
         .finish();
-    tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
-    info!("Booted");
-    dotenv().expect("Failed to load dotenv");
-    rocket::build().mount("/", routes![endpoint])
-}
-
-#[post("/?<hub>")]
-pub fn endpoint(hub: Option<Parameters>) -> Json<Option<challenge::Response>> {
-    dbg!(&hub);
-    match hub {
-        Some(v) => {
-            let verify_token = env::var("VERIFY_TOKEN").unwrap_or(String::new());
-            if v.verify_token != verify_token {
-                info!("Received invalid verify token of {}", verify_token);
-                return Json(None);
-            }
-            Json(Some(challenge::Response {
-                challenge: v.challenge,
-            }))
-        }
-        None => Json(None),
-    }
+    tracing::subscriber::set_global_default(subscriber)
+        .context("setting default subscriber failed")?;
+    Ok(())
 }
