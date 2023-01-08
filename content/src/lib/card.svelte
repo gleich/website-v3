08f75085a4dd1d71ba1f21428f16628db0558116
Matt Gleich <git@mattglei.ch>
2023-01-07 23:35:32 -0500

    feat: polyline demo
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- content/src/lib/card.svelte
+++ content/src/lib/card.svelte
@@ -1,8 +1,7 @@
-<script>
+<script lang="ts">
 	import PolylineSvg from './polylineSVG.svelte';
 
-	const polyline =
-		'o|peGze_tLaAzAi@ZeHb@aBX{@`A}@nCsCdO_DjVcEbUgAzGc@|DyAhTkBtRi@tIMjKJ~K`@nHz@xIrBfMLLdBTbFlCPVc@~Ai@~BEb@a@nAGd@|B`BdGnCZVxBz@nChC~B~C~@pCh@`Cd@vCAr@Jz@?~Fn@pET`AVf@T|@lA~Ar@p@dDtBL@VOd@VHTtB`BxElBrH~AxFvAdPhG|BvAxArBl@rBPfAFfB`@fDd@fCn@fBlAvBnA`BxAdAjCpAvAjAz@|@b@t@p@f@fAtANDj@p@Pl@n@dATz@^pBTl@PhBXtADjACv@A`CSxBDtAGxAK|@[x@rC?PHJEbAs@zFKtB@jA|Dt@zDXdArBlFrBrBf@jAp@~Ab@nBXrEIfF`@xEKzAp@vE`EtAt@nDbAhBV|FLjAVjAd@jA|@t@z@p@fAjBdE`ArAdCzB`DlDtIbGdBrAlBhBrDnE|AbCdAlDhRVtCF|AHrFGdBXdDd@zBr@jBlAtBbAtAvAjAjFjDBVIl@On@SVHDh@gB@_@ISoFwDcA}@_B_CyA_D[iAa@wCIgKSwDi@cEgCgMWoAg@sAaGuHcC_C_FeDmFgEgEkE{BqCiB_Es@kAcAkAoA{@oCy@oHWkFsAkAm@cFgEoAi@i@EmDNeEa@_FJmAIcB]_DoAkAaAaAeBcBqFy@aE_@aEEoAZqFNw@^mERiALUNCnDdAr@@pK{EjKkBlBFNKl@}EtAoH?o@K{@g@u@k@UmG?_AJi@`@Sb@Kf@Ah@xAlQ@hDi@jGtApNLv@[d@w@L}AAkQCmBQgAm@s@mC{@qBi@eEg@i@[g@m@Y{@KgAZuFKkA_@}@uAaBe@_AMi@AiAh@_CB]GM}@@sCf@s@@cHgDKSCoELqBiA]^OdBC`CP|GfCfA~@t@|DLzAGTKF}KdBcAW}EgC_@AITEj@IpDH|@Zr@z@h@hFpAz@r@XKFWEUQKgA@sFuAe@Y[o@K_ANmGCkEViDQg@eCiD]s@g@iBU_BG{BFuDTqAUWUT?TJ`@GnFHnBZjB\fAfBvCpA~ARAlAk@nDB~A`@fGbCp@x@Vp@b@zEGVu@TsJtAq@OcGyCOc@AaD@y@JwAPq@Vc@^Yt@KhEXlHnCp@t@Zx@Lr@VrDQVm@NsJpAs@OgG_DGYAeDJmCNm@Tc@`Ai@vERbGvBx@d@h@l@d@hALlA';
+	export let polyline: string;
 </script>
 
 <div class="container">
--- content/src/routes/+page.svelte
+++ content/src/routes/+page.svelte
@@ -1,6 +1,11 @@
 <script>
 	import Card from '$lib/card.svelte';
 	import Nav from '$lib/index/nav/nav.svelte';
+
+	const polylines = [
+		'pdefAa}}y^HlDxCvFbC`CfEnAzDvF|BvAnH|@fF@xQ{EbHdBhFAd@I`BZf@`Cc@vCr@TYp@aDPc@jAjEhD`BvCZrBm@tFrB`Bp@rBk@lBNrAa@h@gANaDcAwBtB^~AnBvA`@tBkCDf@pCuAz@EdAz@rCjA@rCj@jCzCbC~AdEbDhA~AjBfF`AjBw@lF[dBj@nAdEfCe@jBApCfErEAjCuB`Bj@XvBzClArAC|AaAnAaFoAkBwCaBEgEdCcEtDc@|BmAb@yAKaC_G{BsDv@sCqByBpB}BeCsCI{@~@p@`Cy@xAP|A[x@g@b@cGbAgH{DSs@x@oCsC_CVkDs@oDr@eAnEl@jC|A`BYzBVpBc@`@qCnBT|@g@{@[iC_Bw@s@_BcC]eDZoUaIa@gCgDqBmCPMdC{@fAuFcBaAjC~C~Dc@rCNrAwAx@{DeAoBpBX|AtB~A`@nBoCNl@hCyA`AAjAZp@tClAb@lGdDrCrAtDjDvAlAdBlANv@uDqCcGh@cAlA`@PpCbAdBnB`AhBUCk@aCuArAeHi@mA?eBkAo@l@gASs@^gB~@DPhFrBnEi@bCzAZbAwB?wAt@uBsCiCXaDm@sEnAo@pDt@lC|A`EAvBYf@wCt@UlAb@lAuA_@aD{Am@a@sAuCg@iDVgU{Hc@gCeDsBmCNMdCs@bAeCe@oAqAoCqHgFyDRkA|CMb@uAaDgAmCd@Yc@PuAe@k@}FAiHcB_R|EiFG}HkAcBsAeDeFeGaCuE_HY}D|A_FEcEhAqD]}@gYgHs@yBt@mGu@gAwBhC[vDe@l@u@]IqEk@s@s_@{GA{LdBgFpAiHFgPi@gA_CByZrI}IfAsCs@uG{HuBm@aCH}DtBeCzFiAjAiWbEqDfBqBtCo@jFrA~LqBj@Op@jDbEeJbI[fBP|BpAxDrB~BtPnHpFlLhAPzBe@@iA~@}@uBuAIwBsAy@l@iBbB?hAzCfCzAjD_CCq@oC_CMgBn@c@jCO|Ab@nFxJbMiI|DbFp@ShCcGhGnEj@w@c@qCtBm@b@u@SuAwBwC_@mD{E{@kJd@yDrJuYiGcHb@aOlC[gBNsD~BgBGsDlAcC^kCiCmM|GkAxJoHpMR~An@bA~AV`DaE`GD`Ku@Z{D_AeFjEiM`B_G~A{@lCTlEfATzHwBvLeA~XlGbAu@|AiGr@}@jI[xEh@n@~DtBvCJvAsCnA`@fC]z@{@Q{EyD',
+		'o|peGze_tLaAzAi@ZeHb@aBX{@`A}@nCsCdO_DjVcEbUgAzGc@|DyAhTkBtRi@tIMjKJ~K`@nHz@xIrBfMLLdBTbFlCPVc@~Ai@~BEb@a@nAGd@|B`BdGnCZVxBz@nChC~B~C~@pCh@`Cd@vCAr@Jz@?~Fn@pET`AVf@T|@lA~Ar@p@dDtBL@VOd@VHTtB`BxElBrH~AxFvAdPhG|BvAxArBl@rBPfAFfB`@fDd@fCn@fBlAvBnA`BxAdAjCpAvAjAz@|@b@t@p@f@fAtANDj@p@Pl@n@dATz@^pBTl@PhBXtADjACv@A`CSxBDtAGxAK|@[x@rC?PHJEbAs@zFKtB@jA|Dt@zDXdArBlFrBrBf@jAp@~Ab@nBXrEIfF`@xEKzAp@vE`EtAt@nDbAhBV|FLjAVjAd@jA|@t@z@p@fAjBdE`ArAdCzB`DlDtIbGdBrAlBhBrDnE|AbCdAlDhRVtCF|AHrFGdBXdDd@zBr@jBlAtBbAtAvAjAjFjDBVIl@On@SVHDh@gB@_@ISoFwDcA}@_B_CyA_D[iAa@wCIgKSwDi@cEgCgMWoAg@sAaGuHcC_C_FeDmFgEgEkE{BqCiB_Es@kAcAkAoA{@oCy@oHWkFsAkAm@cFgEoAi@i@EmDNeEa@_FJmAIcB]_DoAkAaAaAeBcBqFy@aE_@aEEoAZqFNw@^mERiALUNCnDdAr@@pK{EjKkBlBFNKl@}EtAoH?o@K{@g@u@k@UmG?_AJi@`@Sb@Kf@Ah@xAlQ@hDi@jGtApNLv@[d@w@L}AAkQCmBQgAm@s@mC{@qBi@eEg@i@[g@m@Y{@KgAZuFKkA_@}@uAaBe@_AMi@AiAh@_CB]GM}@@sCf@s@@cHgDKSCoELqBiA]^OdBC`CP|GfCfA~@t@|DLzAGTKF}KdBcAW}EgC_@AITEj@IpDH|@Zr@z@h@hFpAz@r@XKFWEUQKgA@sFuAe@Y[o@K_ANmGCkEViDQg@eCiD]s@g@iBU_BG{BFuDTqAUWUT?TJ`@GnFHnBZjB\fAfBvCpA~ARAlAk@nDB~A`@fGbCp@x@Vp@b@zEGVu@TsJtAq@OcGyCOc@AaD@y@JwAPq@Vc@^Yt@KhEXlHnCp@t@Zx@Lr@VrDQVm@NsJpAs@OgG_DGYAeDJmCNm@Tc@`Ai@vERbGvBx@d@h@l@d@hALlA'
+	];
 </script>
 
 <div class="root">
@@ -15,6 +20,10 @@
 				>email@mattglei.ch</a
 			>
 			or on <a href="https://twitter.com/matt_gleich" target="_blank" rel="noreferrer">twitter</a>.
+		</div>
+		<div class="cards">
+			<Card polyline={polylines[0]} />
+			<Card polyline={polylines[1]} />
 		</div>
 	</main>
 </div>
