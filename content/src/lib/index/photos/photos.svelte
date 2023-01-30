6399df16b81029c364eecdb25098d7a1ef684943
Matt Gleich <git@mattglei.ch>
2023-01-30 15:25:49 -0500

    feat: photos
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- content/src/lib/index/photos/photos.svelte
+++ content/src/lib/index/photos/photos.svelte
@@ -1,16 +1,36 @@
 <script>
 	import Card from '../card.svelte';
+
+	const filenames = [
+		'spider',
+		'hayden',
+		'vroom',
+		'baseball',
+		'acadia',
+		'moonlight',
+		'lights',
+		'nolan'
+	];
 </script>
 
 <Card>
 	<h2>Photos</h2>
 
-	<img src="/photos/nolan.jpg" alt="nolan" loading="lazy" />
+	<p>
+		I also like to take photos in my free time. Here is some of my favorite photos I've taken over
+		the past few years.
+	</p>
+
+	{#each filenames as filename}
+		<img src={`/photos/${filename}.jpg`} alt="filename" loading="lazy" />
+	{/each}
 </Card>
 
 <style>
 	img {
-		width: 400px;
-		height: auto;
+		max-height: 90vh;
+		width: 100%;
+		object-fit: scale-down;
+		pointer-events: none;
 	}
 </style>
