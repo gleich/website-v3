fc36b3cd715f65e0e801e35ef42a7ee316fb1fb1
Matt Gleich <git@mattglei.ch>
2023-02-25 21:52:59 -0500

    feat: just logo page
    Signed-off-by: Matt Gleich <git@mattglei.ch>

---
+++ content/src/routes/logo/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+	import Logo from '$lib/index/nav/logo.svelte';
+</script>
+
+<main>
+	<Logo class="logo" />
+</main>
+
+<style>
+	:global(.logo) {
+		width: 200px;
+		height: auto;
+	}
+
+	main {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		height: 100vh;
+		width: 100vw;
+	}
+</style>
