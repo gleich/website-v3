7dea0268a707b8ce4898b912cb484635cfcbe05d
Matt Gleich <git@mattglei.ch>
2023-02-25 19:56:23 -0500

    fix: correct mobile view issues
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- content/src/lib/index/strava/graph.svelte
+++ content/src/lib/index/strava/graph.svelte
@@ -7,8 +7,8 @@
 	const totalMovingTime = laps.reduce((partialSum, a) => partialSum + a.moving_time, 0);
 	const data = laps.map((x) => x.average_watts);
 	const maxWattage = Math.max(...data);
-	const totalWidth = 350;
-	const totalHeight = 250;
+	$: totalWidth = 0;
+	const totalHeight = 240;
 
 	let el: HTMLElement;
 
@@ -33,21 +33,40 @@
 	});
 </script>
 
-<div bind:this={el} class="chart" />
+<div class="container" bind:clientWidth={totalWidth}>
+	<div bind:this={el} class="chart" />
+</div>
 
 <style>
 	.chart :global(div) {
 		background-color: #4ea56b;
-		text-align: right;
+		text-align: center;
 		color: white;
-
 		border: solid 1px var(--background);
+		writing-mode: vertical-rl;
+		font-size: 0.7rem;
+		text-orientation: mixed;
 	}
 
 	.chart {
 		display: flex;
 		flex-direction: row;
 		align-items: flex-end;
+		justify-content: center;
+	}
+
+	.container {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		width: 350px;
+		height: 250px;
 		border: solid 1px var(--border);
+	}
+
+	@media (max-width: 400px) {
+		.container {
+			width: 100%;
+		}
 	}
 </style>
