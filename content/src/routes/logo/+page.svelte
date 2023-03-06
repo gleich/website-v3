939b2280be8c540758094edfffa1768f41ed4903
Matt Gleich <git@mattglei.ch>
2023-03-06 14:58:56 -0500

    feat: textured background for logo page
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- content/src/routes/logo/+page.svelte
+++ content/src/routes/logo/+page.svelte
@@ -27,5 +27,8 @@
 		height: 100vh;
 		width: 100vw;
 		background-color: var(--arc-palette-foregroundSecondary);
+		opacity: 0.8;
+		background-image: radial-gradient(white, 0.35px, var(--arc-palette-foregroundSecondary) 0.5px);
+		background-size: 10px 10px;
 	}
 </style>
