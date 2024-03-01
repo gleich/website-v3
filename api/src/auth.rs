25e5a40867a95d6b1fd1188a4cde4e86f35d3fa1
Matt Gleich <git@mattglei.ch>
2024-03-01 01:46:35 -0500

    fix: bump and deps to resolve proc macro

--- api/src/auth.rs
+++ api/src/auth.rs
@@ -17,21 +17,21 @@ impl<'r> FromRequest<'r> for Token {
         let auth_header = match req.headers().get_one("Authorization") {
             Some(x) => x,
             None => {
-                return Outcome::Failure((Status::Unauthorized, String::from("Invalid API token.")))
+                return Outcome::Error((Status::Unauthorized, String::from("Invalid API token.")))
             }
         };
 
         let provided_token = match auth_header.strip_prefix("Bearer ") {
             Some(x) => x,
             None => {
-                return Outcome::Failure((Status::Unauthorized, String::from("Invalid API token.")))
+                return Outcome::Error((Status::Unauthorized, String::from("Invalid API token.")))
             }
         };
 
         let valid_tokens = match env::var("VALID_TOKENS") {
             Ok(x) => x,
             Err(_) => {
-                return Outcome::Failure((Status::Unauthorized, String::from("Invalid API token.")))
+                return Outcome::Error((Status::Unauthorized, String::from("Invalid API token.")))
             }
         };
 
@@ -40,7 +40,7 @@ impl<'r> FromRequest<'r> for Token {
             .collect::<Vec<&str>>()
             .contains(&provided_token)
         {
-            return Outcome::Failure((Status::Unauthorized, String::from("Invalid API token.")));
+            return Outcome::Error((Status::Unauthorized, String::from("Invalid API token.")));
         }
 
         Outcome::Success(Self(String::from(provided_token)))
